e6964f421a3c2d8f2e11cfdc75283d5fe723d818
Дарья Никешина <dashulichka2020@gmail.com>
2025-06-13 19:05:59 +0300

    добавлен sql файл

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,4 +6,22 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS laboratory_works (
+    id_work SERIAL PRIMARY KEY,
+    id_user BIGINT NOT NULL,
+    id_recipient BIGINT NOT NULL,
+    id_departure VARCHAR(36) NOT NULL,
+    id_time TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    file_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    file_size DOUBLE PRECISION NOT NULL,
+    file_type VARCHAR(10) NOT NULL,
+    comment VARCHAR(1000),
+    status VARCHAR(20) NOT NULL,
+    grade INTEGER,
+    is_closed BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_recipient) REFERENCES users(id)
+);
+
 -- Другие таблицы...
