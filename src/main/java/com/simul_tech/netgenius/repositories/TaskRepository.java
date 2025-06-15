b1400846999f4e79e10a50de219a7d5bb6bf9a6c
Dyatlov Egor <Dyatlov-yegor@mail.ru>
2025-06-16 01:05:57 +0300

    Добавлен репозиторий заданий

---
+++ src/main/java/com/simul_tech/netgenius/repositories/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.simul_tech.netgenius.repositories;
+
+import com.simul_tech.netgenius.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByСтатус(String статус);
+    List<Task> findByДатаСозданияBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    List<Task> findByДатаОкончанияBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    List<Task> findByIs_done(boolean is_done);
+    List<Task> findByСтатусAndIs_done(String статус, boolean is_done);
+}
