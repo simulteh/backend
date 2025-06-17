4602dd6f2a300ca0ca058fed5f6fc321775d85da
Dyatlov Egor <Dyatlov-yegor@mail.ru>
2025-06-17 13:25:32 +0300

    Завершение работф команда номер 5

---
+++ src/main/java/com/simul_tech/netgenius/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.simul_tech.netgenius.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("NetGenius API")
+                        .version("1.0")
+                        .description("API для системы управления задачами NetGenius")
+                        .contact(new Contact()
+                                .name("SimulTech")
+                                .email("support@simultech.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+} 
